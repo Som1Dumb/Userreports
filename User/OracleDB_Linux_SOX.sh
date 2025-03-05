52ff869899cb29617c516fecb985e4c5fa63288c
Som1Dumb <wiktor.bernard.krawczyk@gmail.com>
2025-03-05 11:03:44 +0100

    cant test the great python file (copy problems). changing the bash then

--- User/OracleDB_Linux_SOX.sh
+++ User/OracleDB_Linux_SOX.sh
@@ -6,20 +6,20 @@ echo "Running linux_SOX.py..."
 
 # Function to check if Oracle Database is installed
 is_oracle_installed() {
-    # Check for Oracle binary directories or sqlplus
-    if [ -d "/u01/app/oracle" ] || [ -d "/opt/oracle" ] || [ -n "$(which sqlplus 2>/dev/null)" ]; then
-        return 0  # Oracle is installed
-    else
-        return 1  # Oracle is not installed
-    fi
+    oracle_paths=("/etc/oratab" "/u01/app/oracle" "/usr/lib/oracle" "/opt/oracle")
+
+    for path in "${oracle_paths[@]}"; do
+        if [ -e "$path" ]; then
+            return 0  # Oracle is installed
+        fi
+    done
+
+    return 1  # Oracle is not installed
 }
 
 # Run only if Oracle Database is installed
 if is_oracle_installed; then
     echo "Oracle Database detected. Running script..."
-
-    # Extract usernames that match "ora" but not "oracle"
-    USERNAMES=$(awk -F: '/ora/ && !/oracle/ {print $1}' /etc/passwd)
 
     # Create directory if not exists
     mkdir -p /report_tst
